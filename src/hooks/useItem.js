7bdcf92d3b78dfe8b209bfb645396a08d4a32e76
Shlomo Moller <moller.shlomo@gmail.com>
2024-05-22 13:43:25 +0300

    Create useItem

---
+++ src/hooks/useItem.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, useCallback } from "react";
+
+const useItem = id => {
+    const [item, setItem] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchItemInfo = useCallback(async () => {
+        try {
+            const response = await fetch(`https://api.restful-api.dev/objects/${id}`);
+            const data = await response.json();
+            setItem(data);
+            setError(null);
+        } catch (responseError) {
+            console.error(responseError);
+            setError("Item isn't found!");
+        }
+    }, [id]);
+
+    useEffect(async () => {
+        setIsLoading(true);
+        await fetchItemInfo();
+        setIsLoading(false);
+    }, [fetchItemInfo, setIsLoading]);
+
+    return {
+        isLoading,
+        error,
+        item
+    };
+};
+
+export { useItem };
